d1c02dfbb8b5b347adb0096aa9488ff87f9c818e
abal147 <aaron.balsara@gmail.com>
2014-09-22 11:23:35 +1000

    ScraperUtility update

--- server/scraperUtility.py
+++ server/scraperUtility.py
@@ -1,11 +1,13 @@
 # Create a string delimited by delim out of the list words
-def commaString(words, delim):
+def commaString(words, delim = ','):
 	retString = ''
 	for word in words:
 		retString += word + delim
 	return retString[:-len(delim)]
 
-def quoteList(words, quote):
+# Add quotes around each word in a list
+# The quote is specified as a parameter
+def quoteList(words, quote = '"'):
 	retList = []
 	for word in words:
 		retList.append(quote + word + quote)
