23f451c82a42fe4124711bea3fd97ed485829302
Ubuntu <ubuntu@ip-172-31-19-18.ap-southeast-2.compute.internal>
2014-10-21 10:02:38 +0000

    added in some game tables

---
+++ sqldb/user_db.py
@@ -0,0 +1,180 @@
+import sqlite3, time, os
+
+#create a new database and set up the tables
+def createDB():
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	cursor.execute('''
+	CREATE TABLE users
+	(
+		name TEXT,
+		userID INTEGER PRIMARY KEY,
+		email TEXT unqiue,
+		password TEXT
+	)
+	''')
+	#add gameID to this table and where null it is a non-game
+	cursor.execute('''
+	CREATE TABLE transactions
+	(
+		userID INTEGER,
+		stockID TEXT,
+		price REAL,
+		numStocks INTEGER,
+		unixDate INTEGER
+	)
+	''')
+	#userID in the table below is the creator?
+	cursor.execute('''
+	CREATE TABLE games
+	(
+		gameName TEXT,
+		gameID INTEGER PRIMARY KEY,
+		userID INTEGER
+	)
+	''')
+	#list of non-distinct users in games
+	cursor.execute('''
+	CREATE TABLE gameusers
+	(
+		gameID INTEGER,
+		userID INTEGER
+	)
+	''')
+	db.commit()
+	db.close()
+
+#returns true if new user is inserted otherwise if users is not new, returns false
+#userID is automatically generated
+def insertUser(name, email, password):
+	#check if email is unqiue
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	if newUser(email) == 0:
+		return 0	
+	cursor.execute('''
+	INSERT INTO users (name, email, password) values (?,?,?)''', (name, email, password))
+	db.commit()
+	db.close()
+	return 1
+
+#returns true if user is new, else if email is not unique then false
+def newUser(email):
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	cursor.execute(
+	'''SELECT * FROM users WHERE email = (?)''', (email,))
+	if len(cursor.fetchall()) > 0:
+		db.close()
+		return 0
+	db.close()
+	return 1
+
+def getUserID(name):
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	cursor.execute('''SELECT userID FROM users WHERE name = (?)''', (name,))
+	userID = cursor.fetchone()[0]
+	db.close()
+	return userID
+
+def getUser(name):
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	cursor.execute('''SELECT * FROM users WHERE name = (?)''', (name,))
+	userinfo = cursor.fetchall()
+	db.close()
+	return userinfo
+
+#prints all rowsi n the users table
+def printUsers():
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	cursor.execute('''SELECT * FROM users''')
+	for row in cursor:
+		print row[0]
+		print row[1]
+		print row[2]
+		print row[3]
+		print row[4]
+	db.close()
+	return
+
+#inserts a transaction, searches for the userID when given a name
+#userID, stockID, price, numstocks, unixDate
+def insertTrans(inputstr):
+	inputs = inputstr.split(',')
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	userID = getUserID(inputs[0])
+	cursor.execute(
+		'''INSERT INTO transactions (userID, stockID, price, numStocks, unixDate) values (?,?,?,?,?)''', (userID, inputs[1], inputs[2], inputs[3], int(time.time())))
+	db.commit()
+	db.close()
+	return
+
+#just prints out all the transactions in the transaction table
+def printTrans():
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	cursor.execute('''SELECT * FROM transactions''')
+	#return ','.join(cursor)
+	for row in cursor:
+		print row[0]
+		print row[1]
+		print row[2]
+		print row[3]
+	db.close()
+	return
+
+def getTrans(name):
+	db = sqlite3.connect('stock_db.db')
+	userID = getUserID(name)
+	cursor = db.cursor()
+	cursor.execute(
+		'''SELECT * FROM transactions WHERE userID = (?)''', (userID,))
+	transinfo = cursor.fetchall()
+	return transinfo
+
+def getPortfolio(name):
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	userID = getUserID(name)
+	cursor.execute(
+		'''SELECT * FROM transactions WHERE userID = (?)''', (userID,))
+	portfolio = {}
+	for row in cursor:
+		if row[1] in portfolio:
+			portfolio[row[1]] += row[2]*row[3]
+		else:
+			portfolio[row[1]] = row[2]*row[3]
+	returnstr = '['
+	for stockID in portfolio:
+		returnstr += stockID + ':' + str(portfolio[stockID])
+	returnstr += ']'
+	db.close()
+	return str(portfolio)#returnstr
+
+def newGame(gameName, name):
+	db = sqlite3.connect('stock_db.db')
+	cursor = db.cursor()
+	userID = getUserID(name)
+	cursor.execute(
+		'''INSERT INTO games (gameName, userID) values (?,?)''', (gameName, userID))
+	db.commit()
+	db.close()
+	return
+
+#need function to put users into gamestable
+
+os.remove('stock_db.db')
+createDB()
+newUser('asdf')
+insertUser('bob', 'asdf', 'quet')
+newUser('asdf')
+#printUsers()
+insertTrans('bob,wow.ax,40,2.15,13456898763')
+
+printTrans()
+str = getPortfolio('bob')
+print str
