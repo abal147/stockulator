af1580b15499182a9c28b020e273bbd4b6bbd0b6
verted <livelop@hotmail.com>
2014-10-23 05:08:00 +1100

    Added RSS feed and 3 indexes AllOrdss, Consumer and Industrial Indexes

--- myPhonegapApp/www/js/ActionManager.js
+++ myPhonegapApp/www/js/ActionManager.js
@@ -117,6 +117,8 @@ $.validator.setDefaults({
 // This script will be run by all documents to perform manipulation of the page..
 $(document).ready (function(){
 
+
+
 	console.log("Document Manipulation Script has been run");
 	
 	// 1. initialise mobile loader
@@ -144,6 +146,13 @@ $(document).ready (function(){
 // 		setCurrentStock($("#search-3").val());
 // 	});
 		
+	$('#ticker').rssfeed('https://au.finance.yahoo.com/news/category-stocks/?format=rss',{}, function(e) {
+		$(e).find('div.rssBody').vTicker({showItems: 5});
+	});
+
+	plotDataIndicie("^AORD", "ALL ORDINARIES",60,"#indicie1");
+	plotDataIndicie("^AXDJ", "S&P/ASX 200 Consumer Discretionary Index" ,60,"#indicie2");
+	plotDataIndicie("^AXNJ", "S&P/ASX 200 Industrials Index " ,60,"#indicie3");
 
 	$("#buyStock").click(function() {
 		buyWatchStock(getCurrentStock(),1,$("#slider").val());
@@ -236,6 +245,9 @@ $(document).ready (function(){
     	focusInvalid: false
 	});
 	
+
+	
+
 	$("#historyView").click(function(){
 		plotPortHistory();
 	});
